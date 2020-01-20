8586c5a22544a0923fbab993035a99d95870543c
thatkidnicky <43250406+thatkidnicky@users.noreply.github.com>
2020-01-20 16:31:04 -0500

    Add files via upload

--- movies_cms/admin/scripts/read.php
+++ movies_cms/admin/scripts/read.php
@@ -10,3 +10,35 @@ function getAll($tbl){
         return 'There was a problem accessing ths info';
     }
 }
+
+function getSingleMovie($tbl, $col, $id){
+    //TODO: finish function based on getAll, this time only return one movies fields
+
+ $pdo = Database::getInstance()->getConnection();
+ $query = "SELECT * FROM $tbl WHERE $col = $id";
+ $results = $pdo->query($query);
+
+ if($results){
+    return $results;
+}else{
+    return 'There was a problem accessing ths info';
+    }
+
+}
+
+function getMoviesByFilter($args){
+    $pdo = Database::getInstance()->getConnection();
+
+    $filterQuery = 'SELECT * FROM '.$args['tbl'].' AS t, '.$args['tbl2'].' AS t2, '.$args['tbl3'] .' AS t3';
+    $filterQuery .= ' WHERE t.'.$args['col'].' = t3.'.$args['col'];
+    $filterQuery .= ' AND t2.'.$args['col2'].' = t3.'.$args['col2'];
+    $filterQuery .= ' AND t2.'.$args['col3'].' = "'.$args['filter'].'"';
+
+    $results = $pdo->query($filterQuery);
+
+    if($results){
+        return $results;
+    }else{
+        return 'There was a problem accessing ths info';
+        }
+}
---
+++ movies_cms/templates/header.php
@@ -0,0 +1,11 @@
+<header>
+    <h2>This is the Header</h2>
+    <nav class="filterNav">
+        <ul>
+            <li><a href ="index.php?filter=action">Action</a>
+            <li><a href ="index.php?filter=comedy">Comedy</a>
+            <li><a href ="index.php?filter=family">Family</a>
+            <li><a href ="index.php">All</a></li>
+        </ul>
+    </nav>
+</header>
